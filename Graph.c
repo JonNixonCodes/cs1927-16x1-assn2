577d6330478b1669fd07bd1712eaaa9bba109f21
jonnyboy11494 <jonathan.yu.94@gmail.com>
2016-01-30 16:03:25 +1100

    chanes to Graph.c

--- Graph.c
+++ Graph.c
@@ -12,20 +12,41 @@
 
 
 struct graphRep { 
-    //TODO
+  //TODO
+  int nV;
+  int nE;
+  Vertex ** edges;
 }; 
 
 // Create an edge from v to w 
 Edge mkEdge(Vertex v, Vertex w, int weight) { 
-    assert(v >= 0 && w >= 0); 
-    Edge e = {v,w,weight}; 
-    return e; 
-} 
+  assert(v >= 0 && w >= 0); 
+  assert(v != w);
+  Edge e = {v,w,weight}; 
+  return e; 
+}
 
 //Initialise a new graph
 Graph newGraph(int nV) { 
-    //COMPLETE THIS
-    return NULL;
+  //COMPLETE THIS
+  assert(nV >= 0);
+  Graph g = malloc(sizeof(*g)); //allocate memory for graph
+  g->nV = nV;
+  g->nE = 0;
+  g->edges = malloc(sizeof(*(g->edges)) * nV); //allocate memory for pointers to arrays
+  int i = 0;
+  while (i < nV) //allocate memory for each array of vertices
+  {
+    g->edges[i] = malloc(sizeof(Vertex) * nV);
+    int j = 0;
+    while (j < nV) //initialise all weights to -1
+    {
+      g->edges[i][j] = -1;
+      j++;
+    }
+    i++;
+  }
+  return g;
 }
 
 //Insert an edge into a graph
@@ -34,47 +55,81 @@ Graph newGraph(int nV) {
 //     it would create a self loop
 //     there already exists an edge between the vertices
 void insertE(Graph g, Edge e) { 
-    assert(g != NULL); 
-    
-    //COMPLETE THIS
+  assert(g != NULL);
+  //COMPLETE THIS
+  g->edges[e.v][e.w] = e.weight;
+  g->edges[e.w][e.v] = e.weight;
+  g->nE++;
 } 
 
 //returns 1 if there is an edge from v to w
 //returns 0 otherwise
 int isAdjacent(Graph g, Vertex v, Vertex w){
-    //COMPLETE THIS
-    return 0;
+  assert(g != NULL);
+  //COMPLETE THIS
+  if (g->edges[v][w] || g->edges[w][v]) {
+    return 1;
+  }
+  return 0;
 }
 
 //returns the number of adjacent vertices
 //and fills the adj array with the adjacent vertices
 int adjacentVertices(Graph g, Vertex v, Vertex adj[]){
-    //COMPLETE THIS 
-    return 0;
+  assert(g != NULL);
+  //COMPLETE THIS
+  int i = 0; int counter = 0;
+  while (i < g->nV) {
+    if (g->edges[v][i] >= 0) {
+      adj[counter] = i;
+      counter++;
+    }
+    i++;
+  }
+  return counter;
 }
 
 //returns the number of incident edges
 //and fills the edges with incident edges
 int incidentEdges(Graph g, Vertex v, Edge edges[]){
-    //COMPLETE THIS
-    return 0;
+  assert(g != NULL);
+  //COMPLETE THIS
+  int i = 0; int counter = 0;
+  while (i < g->nV) {
+    if (g->edges[v][i] >= 1) {
+      Edge e = mkEdge(v, i, g->edges[v][i]);
+      edges[counter] = e;
+      counter++;
+    }
+    i++;
+  }
+  return counter;
 }
 
 void destroyGraph(Graph g){
-    //COMPLETE THIS
-
+  assert(g != NULL);
+  //COMPLETE THIS
+  int i = 0;
+  while (i < g->nV) {
+    free(g->edges[i]);
+    i++;
+  }
+  free(g->edges);
+  free(g);
 }
 
 //return the number of vertices in the graph
 int numV(Graph g){
-    //COMPLETE THIS
-    return -1;
+  assert(g != NULL);
+  //COMPLETE THIS
+  return g->nV;
 }
 
 //return the number of edges in the graph
 int numE(Graph g){
-    //COMPLETE THIS
-    return -1;
+  assert(g != NULL);
+  //COMPLETE THIS
+  return g->nE;
 }
 
 // returns the number of edges and fills the array of edges.
@@ -82,8 +137,22 @@ int numE(Graph g){
 // The edges in the edges function should be in ascending order and not 
 // contain duplicates. 
 int edges(Edge es[], int nE, Graph g){
-    //COMPLETE THIS
-    return -1;
+  assert(g != NULL);
+  assert(es != NULL);
+  //COMPLETE THIS
+  int i = 0; int counter = 0;
+  while (i < g->nV && counter < nE) {
+    int j = i;
+    while (j < g->nV && counter < nE) {
+      if (g->edges[i][j] >= 0) {
+        es[counter] = mkEdge(i, j, g->edges[i][j]);
+	counter++;
+      }
+      j++;
+    }
+    i++;
+  }
+  return counter;
 }
 
 //Display the graph
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cs1927-16x1-assn2
 Assignment 2: Detectives and Thief
+#T2S0 ver1.00 30_01_16
